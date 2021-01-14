292ef8e3d73040bb00a821254e86c54ed7c71eda
mtttia <tia369852@gmail.com>
2021-01-14 17:54:22 +0100

    global chat

---
+++ server.php
@@ -0,0 +1,58 @@
+<?php
+
+
+    $db = new mysqli('127.0.0.1', 'root', '', 'chatnode');
+
+    
+    if($db->connect_error)
+    {
+        die("Connection failed: " . $db->connect_error);
+    }
+
+    $result = array();
+    $sender = isset($_POST['sender']) ? $_POST['sender'] : null;
+    $message = isset($_POST['message']) ? $_POST['message'] : null;
+
+    if(!empty($message) && !empty($sender))
+    {
+        $sql = "INSERT INTO chat (message, sender) VALUE ('". $message."','". $sender ."')"; 
+        echo $sql;
+        $result['send_status'] = $db->query($sql);
+    }
+
+    //print message
+    $start = isset($_GET['start']) ? intval($_GET['start']) : 0;
+    $sql = "SELECT * FROM chat WHERE id >=" .$start;
+    $r = $db->query($sql);
+    if($r == false)
+    {
+        die('error');
+    }
+    while($row = $r->fetch_assoc()){
+        array_push($result, new Message($row['message'], $row['sender']));
+    }
+
+    $db->close();
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    //header('status-code : 200');
+
+    echo json_encode($result);
+
+    class Message
+    {
+        public $message;
+        public $sender;
+
+        function __construct($mess, $send)
+        {
+            $this->message = $mess;
+            $this->sender = $send;
+        }
+
+
+
+    }
+
+?>
